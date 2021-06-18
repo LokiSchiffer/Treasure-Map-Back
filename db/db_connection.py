098810655e41643e9a82b04a7ac190a4f10205c4
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-06-18 16:54:32 -0500

    Fix db's name for connection

--- db/db_connection.py
+++ db/db_connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/Yggdrassil"
+DATABASE_URL = "postgresql://postgres:root@localhost:5432/Treasure_map"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,
