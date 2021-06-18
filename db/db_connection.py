0da8bd5fa7cf97fcf1f07a3015f9db4d2e0fe1e5
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-06-18 12:19:10 -0500

    Add connection to local db

---
+++ db/db_connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:root@localhost:5432/Yggdrassil"
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False,
+                            autoflush=False,
+                            bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+Base = declarative_base()
+Base.metadata.schema = "firstrun"
