2806a9b702f97933ff105bca02128761f789b5ad
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-06-19 01:07:40 -0500

    Change db url to cloud db

--- db/db_connection.py
+++ db/db_connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/Treasure_map"
+DATABASE_URL = "postgresql://postgres:Ulquiorra91@yggdrassil-cloud.c74snuwjicxg.us-east-2.rds.amazonaws.com:5432/Treasure_map"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,
